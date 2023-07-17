0b663d152e1173655b6aa04a0e79c8d2e31a232b
Namrata0407 <namrataawasthi077@gmail.com>
2023-07-17 18:08:39 +0530

    done

--- evalution_two/avg.py
+++ evalution_two/avg.py
@@ -1,33 +1,21 @@
-def average_age_of_employees(company):
-    total_age = 0
-    count_s_job_titles = 0
+all_company = { 'employees': { 'John': {'age': 35, 'job_title': 'Manager'},
+                         'Emma': {'age': 28, 'job_title': 'Software Engineer'},
+                          'Kelly': {'age': 41, 'job_title': 'Senior Developer'},
+                          'Sam': {'age': 30, 'job_title': 'Software Engineer'}, 
+                          'Mark': {'age': 37, 'job_title': 'Senior Manager'},
+                          'Sara': {'age': 32, 'job_title': 'Software Engineer'},
+                         }
+          }
 
-    employees = company.get('employees', {})
+def averageSal(company):
+  salary = 0
+  initial_count=0
+  for i in company["employees"]:
+   if company["employees"][i]["job_title"][0] == "S":
+     initial_count += 1
+     salary += int(company["employees"][i]["age"])
 
-    for employee, details in employees.items():
-        job_title = details.get('job_title', '').strip().lower()
+  avg = salary/initial_count
+  return avg
 
-        if job_title.startswith('s'):
-            age = details.get('age', 0)
-            total_age += age
-            count_s_job_titles += 1
-
-    if count_s_job_titles == 0:
-        return 0
-
-    average_age = total_age / count_s_job_titles
-    return average_age
-
-# Test case
-company = {
-    'employees': {
-        'John': {'age': 35, 'job_title': 'Manager'},
-        'Emma': {'age': 28, 'job_title': 'Software Engineer'},
-        'Kelly': {'age': 41, 'job_title': 'Senior Developer'},
-        'Sam': {'age': 30, 'job_title': 'Software Engineer'},
-        'Mark': {'age': 37, 'job_title': 'Senior Manager'},
-        'Sara': {'age': 32, 'job_title': 'Software Engineer'},
-    }
-}
-
-print(average_age_of_employees(company))  # Output: 31.0
+print(averageSal(all_company))
